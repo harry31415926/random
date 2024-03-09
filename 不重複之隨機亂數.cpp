59dd41f55e39d3bf7c8d666451cb626a972b148c
harry31415926 <159117016+harry31415926@users.noreply.github.com>
2024-03-09 11:37:48 +0800

    Add files via upload

---
+++ 不重複之隨機亂數.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<cstdlib>
+#include<windows.h>
+using namespace std;
+int main()
+{
+    int arr[9] = { 0 }, x = 0;
+    bool IsCout = false;
+    srand(time(NULL));
+    for (int i = 0; i < 20; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            //cout<<rand()%10<<' ';
+            while (IsCout == false)
+            {
+                x = rand() % 10;
+                if (arr[x] == 0)
+                {
+                    cout << x << ' ';
+                    arr[x] = 1;
+                    IsCout = true;
+                }
+            }
+            IsCout = false;
+        }
+        for (int i = 0; i < 9; i++)
+            arr[i] = 0;
+
+        cout << endl;
+        Sleep(500);
+    }
+    system("pause");
+    return 0;
+}
